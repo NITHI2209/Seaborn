d8463847d181c08bf85287eaa88e6be3b6e2853a
NITHYA SREE S <nithyasree065@gmail.com>
2026-03-19 19:12:11 +0530

    Create Theoritical.py

---
+++ Theoritical.py
@@ -0,0 +1,27 @@
+Introduction to Seaborn
+Seaborn is a Python data visualization library based on Matplotlib.
+It is used to create attractive, informative, and statistical graphics easily.
+
+Features of Seaborn
+1)Built on Matplotlib
+2)Works well with Pandas DataFrame
+3)Provides beautiful default styles
+4)Used for statistical data visualization
+5)Supports complex plots easily
+
+Types of plots in seaborn:
+Line plot->Used to show trend over time.
+Barplot->Used to compare categories.
+Histogram -> Used to show distribution.
+Box plot-> Shows spread of data.
+Scatter plot -> Shows relationship between two variables.
+Heatmap->Shows matrix data using colors.
+Pairplot -> Shows relation between all columns.
+
+Seaborn Themes / Styles
+Types:
+white
+dark
+whitegrid
+darkgrid
+ticks
