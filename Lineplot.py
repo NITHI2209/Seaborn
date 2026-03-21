c5b541bb054ed234c56bf35fcedea0cc952e4796
NITHI2209 <nithyasree065@gmail.com>
2026-03-21 14:18:10 +0530

    created

--- Lineplot.py
+++ Lineplot.py
@@ -5,6 +5,6 @@ import numpy as np
 penguin = sns.load_dataset('penguins')
 sns.set_style("whitegrid")
 sns.set_context("talk")
-sns.lineplot(data= penguin,x="body_mass_g",y="flipper_length_mm",color= "orange")
+sns.lineplot(data= penguin,x="body_mass_g",y="flipper_length_mm",hue="sex",color= "orange")
 plt.show()
 #Refer figure 10
