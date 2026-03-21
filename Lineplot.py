051b2694c3939becfc8577d761075f456a0d4e7f
NITHI2209 <nithyasree065@gmail.com>
2026-03-21 14:15:26 +0530

    created

--- Lineplot.py
+++ Lineplot.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 penguin = sns.load_dataset('penguins')
+sns.set_style("whitegrid")
+sns.set_context("talk")
 sns.lineplot(data= penguin,x="body_mass_g",y="flipper_length_mm",color= "orange")
 plt.show()
 #Refer figure 10
